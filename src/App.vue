f83dea8ffcb7e29b4b1de4596ab64686a15793fa
spenguin10lui <spenguin10@yahoo.com.tw>
2021-11-16 11:50:08 +0800

    環境建置

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <l-map ref="myMap" :zoom="zoom" :center="center" :options="options" style="height: 100vh;">
+      <l-tile-layer :url="url" :attribution="attribution">
+        <l-marker ref="location" :lat-lng="center">
+          <l-popup>
+            現在位置
+          </l-popup>
+        </l-marker>
+      </l-tile-layer>
+    </l-map> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import jsSHA from "jssha";
+
+const GetAuthorizationHeader = () => {
+  const APP_ID = process.env.VUE_APP_APP_ID;
+  const APP_KEY = process.env.VUE_APP_APP_KEY;
+
+  const GMTString = new Date().toGMTString();
+  let ShaObj = new jsSHA('SHA-1', 'TEXT');
+  ShaObj.setHMACKey(APP_KEY, 'TEXT');
+  ShaObj.update('x-date: ' + GMTString);
+  const HMAC = ShaObj.getHMAC('B64');
+  const Authorization = 'hmac username="' + APP_ID + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"';
+
+  return { 'Authorization': Authorization, 'X-Date': GMTString };
+}
 
 export default {
   name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    getData() {
+      const url = "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON";
+      axios({
+        method: 'get',
+        url: url,
+        headers: GetAuthorizationHeader()
+      })
+      .then((response) => {
+        console.log(response);
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  },
   components: {
-    HelloWorld
+
+  },
+  created() {
+    this.getData();
   }
 }
+
 </script>
 
-<style>
+<style lang="scss">
+@import "@/assets/scss/main.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +72,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 300px;
+  height: 300px;
+  background-color: $header;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+
+import { LMap, LTileLayer, LMarker, LPopup, LIcon } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import App from './App.vue';
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+Vue.component("l-popup", LPopup);
+Vue.component("l-icon", LIcon);
+
+// 如果 icon 失效
+// import { Icon } from "leaflet";
+// delete Icon.Default.prototype._getIconUrl;
+// Icon.Default.mergeOptions({
+//   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+//   iconUrl: require("leaflet/dist/images/marker-icon.png"),
+//   shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+// });
 
 Vue.config.productionTip = false
 
